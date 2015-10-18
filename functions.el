6cb3e44ac01b0d62fe0f2d2ec69ccd3f82f0caab
showgood <showgood21@gmail.com>
2015-10-18 15:27:50 +1100

    add functions to replace with current word and selection

--- functions.el
+++ functions.el
@@ -4,3 +4,27 @@
     (erase-buffer)
     (eshell-send-input)))
 
+(defun sum-cua-rectangle ()
+  ;; Treat the content of current cua rectangle as numbers, and
+  ;; calculate sum.
+  (interactive)
+  (message
+   (number-to-string
+    (reduce (lambda (x y) (+ x y))
+            (mapcar (lambda (n) (string-to-number n))
+                    (cua--extract-rectangle))))))
+
+
+; replace current word or selection using vim style for evil mode
+; author: showgood
+(defun evil-replace-word-selection()
+  "replace current word or selection using vim style for evil mode"
+  (interactive)
+  (if (use-region-p)
+      (let (
+            (selection (buffer-substring-no-properties (region-beginning) (region-end))))
+        (if (= (length selection) 0)
+          (message "empty string")
+          (evil-ex (concat "'<,'>s/" selection "/"))
+        ))
+    (evil-ex (concat "%s/" (thing-at-point 'word) "/"))))
